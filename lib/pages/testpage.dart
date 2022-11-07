cd262cdb2396c844b7b952f6928de5ccff90029b
DR. MARUFA KHATUN <nishanbhuinya@gmail.com>
2022-11-07 11:14:37 +0530

    Day 8 starts

--- lib/pages/testpage.dart
+++ lib/pages/testpage.dart
@@ -7,13 +7,86 @@ class Testpage extends StatelessWidget {
   Widget build(BuildContext context) {
     // final Size size = MediaQuery.of(context).size;
     return ListView.builder(
+      itemCount: 2,
+      reverse: false,
       scrollDirection: Axis.vertical,
       itemBuilder: (context, index) => Column(
         children: [
+          ElevatedButton(
+            onPressed: () {},
+            child: Text('Switch'),
+          ),
+          Container(
+            color: Colors.amber,
+            width: 50,
+            height: 50,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            color: Colors.green,
+            width: 32,
+            height: 32,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            color: Colors.blue,
+            width: 250,
+            height: 100,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
           Container(
             color: Colors.amber,
             width: 32,
             height: 32,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            color: Colors.amber,
+            width: 32,
+            height: 32,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            color: Colors.amber,
+            width: 32,
+            height: 32,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            color: Colors.amber,
+            width: 32,
+            height: 32,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            color: Colors.red,
+            width: 32,
+            height: 32,
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Container(
+            color: Colors.red,
+            width: 32,
+            height: 32,
+          ),
+          const SizedBox(
+            height: 20,
           ),
         ],
       ),
