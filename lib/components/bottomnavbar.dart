9f49b45ad960a9e45de51bd8dfe147230160de6b
MNBWorld <nishanbhuinya@gmail.com>
2022-11-12 00:33:03 +0530

    Day 11 passed, bnb almost ready

--- lib/components/bottomnavbar.dart
+++ lib/components/bottomnavbar.dart
@@ -1,7 +1,8 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
+import 'package:mnbpub/pages/coming_soon.dart';
 import 'package:mnbpub/pages/home.dart';
+import 'package:mnbpub/pages/testpage.dart';
 
 class BottomNavBar extends StatefulWidget {
   const BottomNavBar({super.key});
@@ -13,16 +14,9 @@ class BottomNavBar extends StatefulWidget {
 class _BottomNavBarState extends State<BottomNavBar>
     with SingleTickerProviderStateMixin {
   late final AnimationController _controller = AnimationController(
-    duration: const Duration(seconds: 2),
+    duration: const Duration(milliseconds: 300),
     vsync: this,
   );
-  late final Animation<Offset> _offsetAnimation = Tween<Offset>(
-    begin: Offset.zero,
-    end: const Offset(2.65, 0.0),
-  ).animate(CurvedAnimation(
-    parent: _controller,
-    curve: Curves.easeIn,
-  ));
 
   @override
   void dispose() {
@@ -69,12 +63,18 @@ class _BottomNavBarState extends State<BottomNavBar>
                 ),
               ),
               SlideTransition(
-                position: _offsetAnimation,
+                position: Tween<Offset>(
+                  begin: const Offset(-1.0, 0.0),
+                  end: const Offset(1.0, 0.0),
+                ).animate(
+                  CurvedAnimation(
+                    parent: _controller,
+                    curve: Curves.easeInCubic,
+                  ),
+                ),
                 child: Container(
-                  width: size.width / 7.45,
-                  // width: 50,
-                  height: size.height / 15.8,
-                  // height: 50,
+                  width: size.width / 7.4,
+                  height: size.height / 15.5,
                   decoration: BoxDecoration(
                     image: DecorationImage(
                       image: Theme.of(context).brightness == Brightness.light
@@ -89,11 +89,11 @@ class _BottomNavBarState extends State<BottomNavBar>
                 crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
-                  bnbItems('assets/icons/icn1.png', 0, 26.33),
-                  bnbItems('assets/icons/icn2.png', 0, 33),
-                  bnbItems('assets/icons/icn3.png', 0, 34.33),
-                  bnbItems('assets/icons/icn4.png', 0, 18),
-                  bnbItems('assets/icons/icn5.png', 0, 31),
+                  bnbItems('assets/icons/icn1.png', 0, size.height / 25),
+                  bnbItems('assets/icons/icn2.png', 1, size.height / 21),
+                  bnbItems('assets/icons/icn3.png', 2, size.height / 19),
+                  bnbItems('assets/icons/icn4.png', 3, size.height / 34),
+                  bnbItems('assets/icons/icn5.png', 4, size.height / 21),
                 ],
               ),
             ],
@@ -103,21 +103,26 @@ class _BottomNavBarState extends State<BottomNavBar>
     );
   }
 
-  Widget bnbItems(String image, int index, double height) {
+  Widget bnbItems(
+    String image,
+    int index,
+    double height,
+  ) {
     return InkWell(
       splashColor: Theme.of(context).brightness == Brightness.dark
           ? Colors.white.withOpacity(0.5)
           : Colors.pink.withOpacity(0.5),
       enableFeedback: true,
       onTap: () => setState(() {
-        selectedIndex = index;
+        // _selectedIndex = index;
+        _controller.animateTo(index / 5);
       }),
       child: Container(
         alignment: Alignment.center,
         width: 50,
         height: 50,
         child: Padding(
-          padding: const EdgeInsets.only(top: 4.0),
+          padding: const EdgeInsets.only(top: 5.0),
           child: Image.asset(
             image,
             height: height,
